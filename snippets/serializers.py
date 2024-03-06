833516b8e5cba42b90e09d126079985f771c728b
Mostafa <mostafa.saneisn@gmail.com>
2024-03-06 13:06:40 +0330

    Feat: adding User auth to snippets

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,8 +1,19 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField, ReadOnlyField
 from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
+
 
 
 class SnippetSerializer(ModelSerializer):
+    # We could have also used CharField(read_only=True) here.
+    owner = ReadOnlyField(source='owner.username')
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
+
+
+class UserSerializer(ModelSerializer):
+    snippets = PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets']
--- snippets/urls.py
+++ snippets/urls.py
@@ -4,5 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('snippets/', SnippetList.as_view(), name='snippets_list'),
-    path('snippets/<int:pk>', SnippetDetail.as_view(), name='snippet_detail')
+    path('snippets/<int:pk>', SnippetDetail.as_view(), name='snippet_detail'),
+    path('users/', UserList.as_view(), name='user_list'),
+    path('users/<int:pk>', UserDetail.as_view(), name='user_detail'),
 ]
